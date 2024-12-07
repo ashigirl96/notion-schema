06bcc7a6e131ee21ee2d3097bfe2ed46610f1f3f
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2024-12-08 04:03:13 +0900

    add json-schema-to-typescript as submodule

--- src/generate.ts
+++ src/generate.ts
@@ -1,13 +1,14 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import { Client } from '@notionhq/client'
+import type { JSONSchema4 } from 'json-schema'
 import { compile } from 'json-schema-to-typescript'
 import { loadConfig } from './config'
 
-async function generateType(client: Client, databaseId: string, name: string, outputDir: string) {
+async function generateType(client: Client, databaseId: string, name: string, _outputDir: string) {
   const response = await client.databases.retrieve({ database_id: databaseId })
 
-  const schema = {
+  const schema: JSONSchema4 = {
     title: name,
     type: 'object',
     properties: {} as Record<string, object>,
@@ -20,14 +21,11 @@ async function generateType(client: Client, databaseId: string, name: string, ou
         enum: property.select.options.map((option) => option.name),
       }
     } else {
-      schema.properties[key] = { type: 'string' } // 他の型も適宜対応
+      schema.properties[key] = { type: 'st' } // 他の型も適宜対応
     }
   }
 
-  const types = await compile(schema, name)
-  const outputPath = path.resolve(outputDir, `${name}.d.ts`)
-  fs.writeFileSync(outputPath, types)
-  console.log(`Generated type for ${name}: ${outputPath}`)
+  return await compile(schema, name)
 }
 
 export async function generateTypes(configPath: string) {
@@ -41,7 +39,13 @@ export async function generateTypes(configPath: string) {
   }
 
   const entries: [string, string][] = Object.entries(databases)
-  for (const [name, databaseId] of entries) {
-    await generateType(client, databaseId, name, outputDir)
-  }
+  const types = (
+    await Promise.all(
+      entries.map(([name, databaseId]) => {
+        return generateType(client, databaseId, name, outputDir)
+      }),
+    )
+  ).join('\n')
+  const outputPath = path.resolve(outputDir, 'index.d.ts')
+  fs.writeFileSync(outputPath, types)
 }
